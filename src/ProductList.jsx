a88844b7cb32685cfaa4ad4680db0799124dd116
SairajRajput12 <125473711+SairajRajput12@users.noreply.github.com>
2024-10-27 11:39:35 +0530

    updated add to cart logic

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./ProductList.css";
-import CartItem from "./CartItem";
+import CartItem from "./CartItem"; 
+import { addItem } from "./CartSlice";
+import { useDispatch } from "react-redux";
+
 function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addedToCart,setAddedCart] = useState({}); 
-
+  const dispatch = useDispatch(); 
+  
   const plantsArray = [
     {
       category: "Air Purifying Plants",
@@ -282,6 +286,21 @@ function ProductList() {
     e.preventDefault();
     setShowCart(false);
   };
+
+  function handleAddToCart(plant){
+    console.log('clicked');
+    console.log(plant); 
+    dispatch(addItem(plant));
+
+  
+    setAddedCart((prevState) => ({
+      ...prevState,
+      [plant.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+    }));
+
+    console.log(addedToCart)
+
+  }
   return (
     <div>
       <div className="navbar" style={styleObj}>
