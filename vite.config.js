6d8b049d70c9874b37a57c454afe58625a2b658d
SairajRajput12 <125473711+SairajRajput12@users.noreply.github.com>
2024-10-27 15:02:41 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CourseraAssignment/', // Set to your GitHub repo name with slashes
+  base: '/CourseraAssignment' // Use repository name here
 });
