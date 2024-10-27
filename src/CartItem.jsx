52d2afa29de9ae388cb695e2c9e61566f6ee76dc
SairajRajput12 <125473711+SairajRajput12@users.noreply.github.com>
2024-10-27 14:56:19 +0530

    changes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -43,7 +43,6 @@ const CartItem = ({ onContinueShopping }) => {
     }
     else{
       handleRemove(item); 
-    
     }
   };
   
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
-})
+  base: '/CourseraAssignment/', // Set to your GitHub repo name with slashes
+});
